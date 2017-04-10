1970519c83b2b64c09d0ba14100f922c23495e91
alex <alex@staticlibs.net>
2017-04-10 11:08:09 +0100

    1.0.1 docs

--- docs/html/search/all_72.js
+++ docs/html/search/all_72.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['reset',['reset',['../classstaticlib_1_1concurrent_1_1countdown__latch.html#a9d1b59f1f38c7c7b56d43e4bb8ed905d',1,'staticlib::concurrent::countdown_latch']]]
+  ['reset',['reset',['../classstaticlib_1_1concurrent_1_1countdown__latch.html#a9d1b59f1f38c7c7b56d43e4bb8ed905d',1,'staticlib::concurrent::countdown_latch']]],
+  ['resize',['resize',['../classstaticlib_1_1concurrent_1_1growing__buffer.html#a451712f43b130803c0e471c0f2a3aa23',1,'staticlib::concurrent::growing_buffer']]]
 ];
--- docs/html/search/functions_67.js
+++ docs/html/search/functions_67.js
@@ -1,5 +1,5 @@
 var searchData=
 [
   ['get_5fcount',['get_count',['../classstaticlib_1_1concurrent_1_1countdown__latch.html#a49fec0466e2a5d37ee716d2d3ef57059',1,'staticlib::concurrent::countdown_latch']]],
-  ['growing_5fbuffer',['growing_buffer',['../classstaticlib_1_1concurrent_1_1growing__buffer.html#afb87d73309e46463c2d63e6ad3fe4573',1,'staticlib::concurrent::growing_buffer::growing_buffer(const char *buffer, size_t length)'],['../classstaticlib_1_1concurrent_1_1growing__buffer.html#a8f755ded2db1cc477edc96df59b9f987',1,'staticlib::concurrent::growing_buffer::growing_buffer(const growing_buffer &amp;other)']]]
+  ['growing_5fbuffer',['growing_buffer',['../classstaticlib_1_1concurrent_1_1growing__buffer.html#a6f3179539dddf181826d1e13c64cbeea',1,'staticlib::concurrent::growing_buffer::growing_buffer()'],['../classstaticlib_1_1concurrent_1_1growing__buffer.html#a8f755ded2db1cc477edc96df59b9f987',1,'staticlib::concurrent::growing_buffer::growing_buffer(const growing_buffer &amp;other)']]]
 ];
