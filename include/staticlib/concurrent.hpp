6372cbd2a7822910ab3439240b5eac33a3035d39
alex <alex@staticlibs.net>
2017-05-18 21:52:41 +0100

    cancelable timer added

--- include/staticlib/concurrent.hpp
+++ include/staticlib/concurrent.hpp
@@ -24,6 +24,7 @@
 #ifndef STATICLIB_CONCURRENT_HPP
 #define	STATICLIB_CONCURRENT_HPP
 
+#include "staticlib/concurrent/cancelable_timer.hpp"
 #include "staticlib/concurrent/condition_latch.hpp"
 #include "staticlib/concurrent/countdown_latch.hpp"
 #include "staticlib/concurrent/growing_buffer.hpp"
