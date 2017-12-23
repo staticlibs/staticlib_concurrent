a201326669569321f1bf2fb99ce0edc1a63bd2a1
alex <alex@staticlibs.net>
2017-12-23 16:39:11 +0000

    1.0.2 docs

--- docs/html/search/classes_63.js
+++ docs/html/search/classes_63.js
@@ -1,5 +1,6 @@
 var searchData=
 [
+  ['cancelable_5ftimer',['cancelable_timer',['../classstaticlib_1_1concurrent_1_1cancelable__timer.html',1,'staticlib::concurrent']]],
   ['condition_5flatch',['condition_latch',['../classstaticlib_1_1concurrent_1_1condition__latch.html',1,'staticlib::concurrent']]],
   ['countdown_5flatch',['countdown_latch',['../classstaticlib_1_1concurrent_1_1countdown__latch.html',1,'staticlib::concurrent']]]
 ];
--- docs/html/search/files_63.js
+++ docs/html/search/files_63.js
@@ -1,5 +1,6 @@
 var searchData=
 [
+  ['cancelable_5ftimer_2ehpp',['cancelable_timer.hpp',['../cancelable__timer_8hpp.html',1,'']]],
   ['concurrent_2ehpp',['concurrent.hpp',['../concurrent_8hpp.html',1,'']]],
   ['condition_5flatch_2ehpp',['condition_latch.hpp',['../condition__latch_8hpp.html',1,'']]],
   ['countdown_5flatch_2ehpp',['countdown_latch.hpp',['../countdown__latch_8hpp.html',1,'']]]
